b816a60b88069d0671a63fd96d44a75b4d4f661c
gotaehyuk <gotaehyuk@gmail.com>
2017-11-22 11:51:06 +0900

    g

--- app/src/main/java/com/example/tidy1/reflectnote/LoadingActivity.java
+++ app/src/main/java/com/example/tidy1/reflectnote/LoadingActivity.java
@@ -14,7 +14,7 @@ public class LoadingActivity extends AppCompatActivity {
         Handler handler= new Handler();
         handler.postDelayed(new Runnable(){
             public void run(){
-                Intent intent = new Intent(getBaseContext(),MainActivity.class);
+                Intent intent = new Intent(getBaseContext(),LoginActivity.class);
             startActivity(intent);
             finish();}
                             }
